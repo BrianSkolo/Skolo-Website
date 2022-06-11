c2456af085a18437d09df045e782d6feeced88af
BrianM <brianmillarsd@gmail.com>
2022-06-11 13:11:23 -0700

    Skolo site commit 6/11 2

--- pages/connect.js
+++ pages/connect.js
@@ -1,31 +1,50 @@
 import styles from '../styles/Home.module.css';
+import Link from "next/link";
 
 // 
 
 export default function Form() {
   return (
     <div className={styles.connect}>
-      <div className={styles.connectContactUs}>
-        <h1>Contact Us:</h1>
+
+      {/* tour dates */}
+      <div style={{ marginBottom: '100px' }}>
+          <h1 className={styles.tourDates}>
+            Tour Dates
+          </h1>
+          {/* purchase tickets */}
+          <Link class="shows" href="/shows">
+                <button className={styles.buttonbg} style={{ borderWidth: '1px', borderStyle: 'solid', padding: '5px', borderRadius: '4px'}}>Purchase Tickets</button>
+          </Link>
       </div>
 
-      <div className={styles.connectContactForm} style={{ display: 'flex', justifyContent: 'center' }}>
+      {/* email header */}
+      <div className={styles.connectContactUs} >
+        <h1 style={{ display: 'flex', justifyContent: 'center', alignContent: 'center' }}>Community News:</h1>
+        <h2 style={{ display: 'flex', justifyContent: 'center', alignContent: 'center' }}>Subscribe with your email address to keep up with the latest Skolo news, events, and exclusives via email.</h2>
+      </div>
+
+
+      {/* contact form */}
+      <div className={styles.connectContactForm} style={{ display: 'flex', justifyContent: 'center', alignContent: 'center' }}>
+
         <form className={styles.form} action="/api/form" method="post">
-          <label htmlFor="first">First Name</label>
+          {/* <label htmlFor="first">First Name</label>
           <input className="input" type="text" id="first" name="first" required />
 
           <label htmlFor="second">Last Name</label>
-          <input className="input" type="text" id="last" name="last" required />
+          <input className="input" type="text" id="last" name="last" required /> */}
 
-          <label htmlFor="third">Email</label>
-          <input className="input" type="text" id="email" required />
+          <label htmlFor="third">
+            <input className="input" type="text" id="email"  style={{  borderRadius: '4px' }} placeholder='Enter your email'/>
+          </label>
 
-          <label htmlFor="last">Message</label>
-          <input className="input" type="text" id="email" required />
+          {/* <label htmlFor="last">Message</label>
+          <input className="input" type="text" id="email" required /> */}
 
           <br />
 
-          <button className="input" style={{width: '25%'}} type="submit">Submit</button>
+          <button className={styles.form} style={{width: '4%', borderRadius: '4px'}} type="submit">Sign Up</button>
         </form>
       </div>
     </div>
